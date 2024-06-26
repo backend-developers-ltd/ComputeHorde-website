db33faf95ced91a2c7f0ef83493e4eb580b5e6c5
Maciej Urbanski <maciej.rooter.urbanski@reef.pl>
2024-06-26 22:17:33 +0200

    nicer error message for no miner available in admin

--- app/src/project/core/admin.py
+++ app/src/project/core/admin.py
@@ -205,6 +205,8 @@ class JobAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         try:
             super().save_model(request, obj, form, change)
+        except Miner.DoesNotExist:
+            self.message_user(request, "No active miners available", level=messages.ERROR)
         except Validator.DoesNotExist:
             self.message_user(request, "No active validators available", level=messages.ERROR)
 
